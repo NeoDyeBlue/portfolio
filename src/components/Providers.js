1795c0dd5c3492fde6c9586454cc63112e8d1f4f
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2023-07-14 14:14:32 +0800

    added custom cursor

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,6 +1,7 @@
 "use client";
 import { ThemeProvider } from "next-themes";
 import { useState, useEffect } from "react";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function Providers({ children }) {
   const [mounted, setMounted] = useState(false);
@@ -11,5 +12,27 @@ export default function Providers({ children }) {
   if (!mounted) {
     return <>{children}</>;
   }
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+          mixBlendMode: "exclusion",
+          zIndex: 999,
+        }}
+        outerStyle={{
+          border: "2px solid var(--cursor-color)",
+          mixBlendMode: "exclusion",
+          zIndex: 999,
+        }}
+      />
+      {children}
+    </ThemeProvider>
+  );
 }
