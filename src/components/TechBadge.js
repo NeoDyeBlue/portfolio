ca51c9d0e50d1154f10fcd4655de43c1fe190618
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2023-07-15 18:58:48 +0800

    Update TechBadge.js

--- src/components/TechBadge.js
+++ src/components/TechBadge.js
@@ -1,6 +1,6 @@
 export default function TechBadge({ text = "" }) {
   return (
-    <span className="rounded-full border border-gray-300 px-3 py-1 text-xs font-semibold">
+    <span className="rounded-full border border-gray-300 px-3 py-1 text-xs font-semibold dark:border-gray-500">
       {text}
     </span>
   );
