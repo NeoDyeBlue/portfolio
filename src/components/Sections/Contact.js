41666e9ec4cb91fd4740869eb5d259e240548ca0
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2023-07-21 14:09:15 +0800

    Update Contact.js
    added gradient layer

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -9,8 +9,8 @@ export default function Contact() {
   return (
     <div
       id="contact"
-      className="relative -mx-6 -mb-6 flex h-[calc(100vh-(1.5rem*2))] flex-col justify-between gap-10 rounded-t-[20px] bg-[url('/nahida-cafe-day.gif')] bg-cover bg-[-58%_center] p-6 text-white
-      dark:bg-[url('/nahida-cafe-night.gif')] 540px:bg-[-103%_center] md:mx-0 md:mb-0 md:h-[calc(100vh-3rem-1px)] md:bg-[-80%_center] md:pb-12 lg:bg-right"
+      className="relative -mx-6 -mb-6 flex h-[calc(100vh-(1.5rem*2))] flex-col justify-between gap-10 rounded-t-[20px] bg-[linear-gradient(0deg,rgba(0,0,0,1)0%,rgba(0,0,0,0)50%),url('/nahida-cafe-day.gif')] bg-cover bg-[-58%_center] p-6 text-white
+      dark:bg-[linear-gradient(0deg,rgba(0,0,0,1)0%,rgba(0,0,0,0)50%),url('/nahida-cafe-night.gif')] 540px:bg-[-103%_center] md:mx-0 md:mb-0 md:h-[calc(100vh-3rem-1px)] md:bg-[-80%_center] md:pb-12 lg:bg-right"
     >
       <div className="flex flex-col gap-6 drop-shadow-lg md:max-w-[400px]">
         <p className="font-display text-2xl font-bold italic">
